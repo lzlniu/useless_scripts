c35aa7d9bafaabab4b0ab7d6af2cc76c10f965a8
lzlniu <lzlniu@qq.com>
2020-04-07 22:33:07 +0800

    update igem.sh

--- igem.sh
+++ igem.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #author:Zelin Li
 #date:2020/04/06
+#usage:bash igem.sh igem2019.html
 
-grep "<a href=\"https://igem.org/Team.cgi?id=" igem2019.html | sed -e 's/ //g' | sed -e 's/<ahref=\"//g' | sed -e 's/style=\"font-size:12px;line-height:15px;font-weight:600;color:blue;\">//g' | sed -e 's/<\/a>//g' | sed -e 's/"/\t/g' > list
+input_igem_page=$1
+#you have to save the igeam team page as igemXXXX.html. For example, igem 2019 track list could be from https://igem.org/Team_Tracks?year=2019 (using chrome)
+grep "<a href=\"https://igem.org/Team.cgi?id=" ${input_igem_page} | sed -e 's/ //g' | sed -e 's/<ahref=\"//g' | sed -e 's/style=\"font-size:12px;line-height:15px;font-weight:600;color:blue;\">//g' | sed -e 's/<\/a>//g' | sed -e 's/"/\t/g' > list
 awk -F '\t' '{print $1}' list > site
 line=1
 for i in $(cat site); do
@@ -13,4 +16,3 @@ for i in $(cat site); do
 done
 rm -rf list
 rm -rf site
-
